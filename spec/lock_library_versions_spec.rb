3c32d7415b5b2c8a1fa5865f5adc5d6469e0c5e1
Takuma Homma <nagomimatcha@gmail.com>
2017-11-19 20:13:18 +0900

    Add spec

--- spec/lock_library_versions_spec.rb
+++ spec/lock_library_versions_spec.rb
@@ -2,14 +2,34 @@ require File.expand_path("../spec_helper", __FILE__)
 
 module Danger
   describe Danger::DangerLockLibraryVersions do
+    let(:git_plugin) { Danger::DangerfileGitPlugin }
+    let(:dangerfile) { testing_dangerfile }
+    let(:plugin) { dangerfile.lock_library_versions }
+
     it "should be a plugin" do
       expect(Danger::DangerLockLibraryVersions < Danger::Plugin).to be_truthy
     end
 
-    describe "with Dangerfile" do
+    describe "#check" do
       before do
-        @dangerfile = testing_dangerfile
-        @my_plugin = @dangerfile.lock_library_versions
+        allow_any_instance_of(git_plugin).to receive(:modified_files).and_return(modified_files)
+      end
+
+      context 'not included' do
+        let(:modified_files) { ['README.md'] }
+        it do
+          plugin.check
+          expect(dangerfile.status_report[:errors][0]).to be_nil
+        end
+      end
+
+      context 'included' do
+        let(:modified_files) { ['README.md', 'Gemfile'] }
+        it do
+          plugin.check
+          expect(dangerfile.status_report[:errors].length).to be == 1
+          expect(dangerfile.status_report[:errors][0]).to eq 'Gemfile.lock should be committed'
+        end
       end
     end
   end
