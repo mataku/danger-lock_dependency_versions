f07776b0f428a5b14bb5a19b64d7e1a516ff6793
Takuma Homma <nagomimatcha@gmail.com>
2017-11-19 21:01:34 +0900

    Add specs

--- spec/lock_library_versions_spec.rb
+++ spec/lock_library_versions_spec.rb
@@ -15,20 +15,42 @@ module Danger
         allow_any_instance_of(git_plugin).to receive(:modified_files).and_return(modified_files)
       end
 
-      context 'not included' do
+      context 'files which managed library versions are not included' do
         let(:modified_files) { ['README.md'] }
-        it do
+
+        it 'no errors' do
           plugin.check
-          expect(dangerfile.status_report[:errors][0]).to be_nil
+          expect(dangerfile.status_report[:errors].length).to be == 0
         end
       end
 
-      context 'included' do
+      context 'file which managed library versions are included' do
         let(:modified_files) { ['README.md', 'Gemfile'] }
-        it do
+
+        it 'error' do
           plugin.check
           expect(dangerfile.status_report[:errors].length).to be == 1
           expect(dangerfile.status_report[:errors][0]).to eq 'Gemfile.lock should be committed'
+        end
+      end
+
+      context 'files which managed library versions are included' do
+        let(:modified_files) { ['Cartfile', 'Gemfile'] }
+
+        it 'multiple errors' do
+          plugin.check
+          expect(dangerfile.status_report[:errors].length).to be == 2
+          expect(dangerfile.status_report[:errors][0]).to eq 'Gemfile.lock should be committed'
+          expect(dangerfile.status_report[:errors][1]).to eq 'Cartfile.resolved should be committed'
+        end
+      end
+
+      context 'with lock file' do
+        let(:modified_files) { ['Gemfile', 'Gemfile.lock'] }
+
+        it 'no errors' do
+          plugin.check
+          expect(dangerfile.status_report[:errors].length).to be == 0
         end
       end
     end
